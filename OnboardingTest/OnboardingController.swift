95bed6cbcbd9bc2cee5ec1715d577bf80402363d
Andrew Frolenkov <andrewissuess@yahoo.com>
2024-01-05 06:09:43 +0300

    Create WelcomeScreenController and add images

--- OnboardingTest/OnboardingController.swift
+++ OnboardingTest/OnboardingController.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 
+// MARK: - OnboardingController
 final class OnboardingController: UIViewController {
 
+  // MARK: - Private Property
+  
+  // MARK: - Override Methods
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.backgroundColor = .red
+    setupView()
   }
 
 
 }
 
+// MARK: - Setting Views
+private extension OnboardingController {
+  func setupView() {
+    settingSuperview()
+    addSubview()
+    
+    setuplayout()
+  }
+  
+  func settingSuperview() {
+    view.backgroundColor = .red
+  }
+  
+  
+  
+}
+
+// MARK: - Setting
+private extension OnboardingController {
+  func addSubview() {
+    
+  }
+}
+
+// MARK: - Layout
+private extension OnboardingController {
+  func setuplayout() {
+    
+  }
+}
